e2161f377f610b771c926d7c52a05515623fdb93
syaning <syaningv@gmail.com>
2016-12-26 11:22:30 +0800

    question api ok

--- zhihuapi/parser/question.py
+++ zhihuapi/parser/question.py
@@ -1,7 +1,7 @@
 from pyquery import PyQuery as pq
 
 from .. import urls
-from .util import parse_slug, parse_int, parse_float
+from .util import parse_slug, parse_int
 
 
 def answers_byvote(htmls):
@@ -11,6 +11,11 @@ def answers_byvote(htmls):
         ele = d('.zm-item-answer')
         answers.append(_parse_answer(d, ele))
     return answers
+
+
+def answers_bypage(d):
+    eles = d('#zh-question-answer-wrap .zm-item-answer')
+    return [_parse_answer(d, d(ele)) for ele in eles]
 
 
 def _parse_answer(d, ele):
@@ -36,3 +41,64 @@ def _parse_answer(d, ele):
     }
     answer['author'].update(parse_slug(author_link))
     return answer
+
+
+def detail(d):
+    main_ele = d('.zu-main-content-inner')
+    visits_ele = d('meta[itemprop="visitsCount"]', main_ele)
+    topics_ele = d('.zm-tag-editor-labels .zm-item-tag', main_ele)
+    title_ele = d('#zh-question-title .zm-item-title', main_ele)
+    detail_ele = d('#zh-question-detail .zm-editable-content', main_ele)
+    if not len(detail_ele):
+        detail_ele = d('#zh-question-detail textarea', main_ele)
+    comments_ele = d('#zh-question-meta-wrap .toggle-comment', main_ele)
+    answers_ele = d('#zh-question-answer-num', main_ele)
+    sidebar_ele = d('.zu-main-sidebar')
+    follow_btn_ele = d('.follow-button', sidebar_ele)
+    follow_ele = d('.zh-question-followers-sidebar .zg-gray-normal a',
+                   sidebar_ele)
+    link = follow_ele.attr('href')[:-len('/followers')]
+
+    question = {
+        'id': parse_int(link[len('/question/')]),
+        'type': 'question',
+        'title': title_ele.text().strip(),
+        'url': urls.full(link),
+        'content': detail_ele.text(),
+        'answer_count': parse_int(answers_ele.attr('data-num')),
+        'comment_count': parse_int(comments_ele.text().split()[0]),
+        'follower_count': parse_int(follow_ele.text()),
+        'visit_count': parse_int(visits_ele.attr('content')),
+        'topics': [_parse_topics(d(ele)) for ele in topics_ele]
+    }
+    return question
+
+
+def _parse_topics(ele):
+    link = ele.attr('href')
+    topic = {
+        'id': parse_int(link[len('/topic/'):]),
+        'type': 'topic',
+        'name': ele.text().strip()
+    }
+    return topic
+
+
+def followers(html):
+    d = pq(html)
+    eles = d('.zm-profile-card')
+    return [_parse_follower(d, ele) for ele in eles]
+
+
+def _parse_follower(d, ele):
+    id_ele = d('.zm-rich-follow-btn', ele)
+    link_ele = d('.zm-item-link-avatar', ele)
+    link = link_ele.attr('href')
+
+    follower = {
+        'id': id_ele.attr('data-id'),
+        'name': link_ele.attr('title'),
+        'url': urls.full(link),
+    }
+    follower.update(parse_slug(link))
+    return follower
--- zhihuapi/question.py
+++ zhihuapi/question.py
@@ -30,3 +30,48 @@ class Question:
         }
         r = req.post(url, data=data)
         return parser.answers_byvote(r['msg'])
+
+    def answers_bypage(self, page=1):
+        """Get answers by page (or created time).
+
+        Args:
+            page: Page number.
+
+        Returns:
+            A list of answers.
+        """
+        url = '/question/%d' % self.id
+        params = {
+            'page': page,
+            'sort': 'created'
+        }
+        d = req.get(url, params)
+        return parser.answers_bypage(d)
+
+    def detail(self):
+        """Get detail information of this question.
+
+        Returns:
+            Detailed question information.
+        """
+        url = '/question/%d' % self.id
+        d = req.get(url)
+        return parser.detail(d)
+
+    def followers(self, offset=0):
+        """Get users that followed this question.
+
+        Args:
+            offset: An integer.
+
+        Returns:
+            A list of users.
+        """
+        url = '/question/%d/followers' % self.id
+        data = {
+            'offset': offset,
+            'start': 0,
+            '_xsrf': req._xsrf
+        }
+        r = req.post(url, data=data)
+        return parser.followers(r['msg'][1])
